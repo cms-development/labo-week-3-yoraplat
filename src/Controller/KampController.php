3cad4f46c03b109b739370cc687108829b4dfadd
Yoram <yoraplat@student.arteveldehs.be>
2019-10-14 20:56:16 +0200

    added translations

--- src/Controller/KampController.php
+++ src/Controller/KampController.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
+
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
